7af6ec9884f3bdf4261e9456f43ea8b9a43d3b30
Bongo <132151115+Bongoros@users.noreply.github.com>
2024-08-31 20:53:46 +0200

    Add files via upload

---
+++ .gitattributesDownload.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Your .gitattributes File Is Downloading!
+curl https://raw.githubusercontent.com/Bongoros/OtherStuff/main/.gitattributes -o .gitattributes
+echo Your .gitattributes File Was Successfully Downloaded!
+pause
